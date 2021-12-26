761ccb16f9463075715e4dfa0208fb9c7d420349
Zoé <zoepesleuxzp@gmail.com>
2021-12-26 19:15:27 +0100

    Added SideDrawer Menu and charactersList page

---
+++ lib/widgets/side_drawer.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'characters_list_page.dart';
+
+class SideDrawer extends StatelessWidget {
+  const SideDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 0.65,
+      child: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'D&D',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.people),
+              title: const Text('Characters'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CharactersListPage(),
+                  ),
+                );
+              },
+            ),
+            const ListTile(
+              leading: Icon(Icons.crop_square),
+              title: Text('Dices'),
+            ),
+            const ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
