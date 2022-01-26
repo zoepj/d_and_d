b64cf19124be27a555595d03a4545b6b7a31f3c6
Isabel <isadede@alum.us.es>
2022-01-26 16:29:57 +0100

    enumerates

--- lib/models/spell.dart
+++ lib/models/spell.dart
@@ -1,7 +1,11 @@
+import 'package:d_and_d/models/damageType.dart';
+import 'package:d_and_d/models/school.dart';
+
 class Spell {
   int level;
   String name;
-  String type;
+  DamageType damageType;
+  School school;
   String castingTime;
   String range;
   String components;
@@ -11,7 +15,8 @@ class Spell {
   Spell(
       {this.level = 0,
         required this.name,
-        this.type = "",
+        this.damageType = DamageType.ACID,
+        this.school = School.ABJURATION,
         this.castingTime = "",
         this.range = "",
         this.components = "",
@@ -22,7 +27,8 @@ class Spell {
     return Spell(
         name: json['name'],
         level: json['level'],
-        type: json['type'],
+        damageType: json['damageType'],
+        school: json['school'],
         castingTime: json['castingTime'],
         range: json['range'],
         components: json['components'],
@@ -35,7 +41,8 @@ class Spell {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['level'] = this.level;
     data['name'] = this.name;
-    data['type'] = this.type;
+    data['damageType'] = this.damageType;
+    data['school'] = this.school;
     data['castingTime'] = this.castingTime;
     data['range'] = this.range;
     data['components'] = this.components;
