44d7b1ecd68d783be50b22db4393396c6f343028
Isabel <isadede@alum.us.es>
2022-02-02 02:20:08 +0100

    tests

--- test/equipment_test.dart
+++ test/equipment_test.dart
@@ -1,4 +1,8 @@
 import 'package:d_and_d/main.dart';
+import 'package:d_and_d/models/armor.dart';
+import 'package:d_and_d/models/character.dart';
+import 'package:d_and_d/models/my_object.dart';
+import 'package:d_and_d/models/weapon.dart';
 import 'package:d_and_d/persistency/shared_preferences_db.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -13,6 +17,11 @@ void main() {
     DB.clear();
     await tester.pumpWidget(const MyApp());
 
-    
+    Armor a = Armor(name: "a", id: 1);
+    Weapon w = Weapon(name: "w", id: 1);
+    //MyObject o = MyObject(name: "o", id: 1);
+    Character c = Character(name: "c", id: 1, armors: [a], weapons: [w], objects: [], spells: []);
+    DB.addCharacter(c);
+
   });
 }
